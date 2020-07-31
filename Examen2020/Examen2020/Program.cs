977c1dfecddeb7eaaf7434178808ec63d057750b
Craituz <68795431+Craituz@users.noreply.github.com>
2020-07-30 21:10:16 -0500

    Add files via upload

---
+++ Examen2020/Examen2020/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Examen2020;
+
+namespace Examen2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           /* Furgoneta furgoneta;
+            List<Furgoneta> list = new List<Furgoneta>();
+            list.Add(new Furgoneta(2,200,45));*/
+
+            List<Vehiculo> lista = new List<Vehiculo>();
+            lista.Add(new Vehiculo("Honda", "Arx2011", "2012"));
+            lista.Add(new Vehiculo("Honda", "Arx2011", "2012"));
+            lista.Add(new Vehiculo("Honda", "Arx2011", "2012"));
+            lista.Add(new Vehiculo("Honda", "Arx2011", "2012"));
+
+            foreach (var vehiculo in lista)
+            {
+                Console.WriteLine(""+lista);
+            }
+        }
+    }
+}
---
+++ Examen2020/Examen2020/Vehiculo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen2020
+{
+    class Vehiculo
+    {
+        public Vehiculo(string marca, string modelo, string año)
+        {
+            Marca = marca;
+            Modelo = modelo;
+            Año = año;
+        }
+
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Año { get; set; }
+        
+    }
+}
